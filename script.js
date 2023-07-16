94688b508800695d4a767dd446b10076555e3c3b
jaeyjeon <n8720432@naver.com>
2023-07-17 02:42:11 +0900

    add top paddle & score

--- script.js
+++ script.js
@@ -3,21 +3,47 @@ var ctx = canvas.getContext("2d");
 
 var ballRadius = 10;
 var x = canvas.width / 2;
-var y = canvas.height - 30;
+var y = canvas.height / 2;
 var dx = 4;
 var dy = -4;
 var color = ["blue" , "red" , "yellow", "green"];
 var hit = 0;
 
 var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleWidth = 100;
 var paddleX = (canvas.width - paddleWidth) / 2;
+var paddleX2 = (canvas.width - paddleWidth) / 2;
 
 var rightPressed = false;
 var leftPressed = false;
 
+var scoreTop = 0;
+var scoreBottom = 0;
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function drawScore(){
+	ctx.font = "24px Arial";
+	ctx.fillStyle = "black";
+	ctx.fillText("Bottom Score: " + scoreBottom, 8, canvas.height - 20);
+	ctx.fillText("Top Score: " + scoreTop, 8, 20);
+}
+
+function wait(sec) {
+	let start = Date.now(), now = start;
+	while (now - start < sec * 1000) {
+		now = Date.now();
+	}
+}
+
+function mouseMoveHandler(e){
+	var relativeX = e.clientX - canvas.offsetLeft;
+	if (relativeX > 0 && relativeX < canvas.width) {
+		paddleX2 = relativeX - paddleWidth/2;
+	}
+}
 
 function keyDownHandler(e){
 	if (e.keyCode == 39){
@@ -37,9 +63,17 @@ function keyUpHandler(e){
 	}
 }
 
-function drawPaddle(){
+function drawPaddleBottom(){
 	ctx.beginPath();
 	ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function drawPaddleTop(){
+	ctx.beginPath();
+	ctx.rect(paddleX2, 0, paddleWidth, paddleHeight);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -57,36 +91,43 @@ function changeBallPosition(){
 	x += dx;
 	y += dy;
 	if (x >= canvas.width - ballRadius || x <= ballRadius)
-	{
-		if (dx > 0)
-			dx = -4 - hit % 3;
-		else
-			dx = 4 + hit % 3;
-		hit++;
-	}
+		dx = -dx;
 	if (y <= ballRadius)
 	{
-		dy *= -1;
-		hit++;
-	}
-	else if(y > canvas.height - ballRadius - paddleHeight) {
-		if (x + ballRadius > paddleX && x - ballRadius < paddleX + paddleWidth)
+		if (x >= paddleX2 && x <= paddleX2 + paddleWidth)
 			dy = -dy;
 		else{
-			document.location.reload();
+			x = canvas.width / 2;
+			y = canvas.height / 2;
+			scoreBottom++;
+			wait(2);
 		}
+		hit++;
+	}
+	else if(y >= canvas.height - ballRadius) {
+		if ((x >= paddleX && x <= paddleX + paddleWidth))
+			dy = -dy;
+		else{
+			x = canvas.width / 2;
+			y = canvas.height / 2;
+			scoreTop++;
+			wait(2);
+		}
+		hit++;
 	}
 }
 
 function draw(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
-	drawPaddle();
+	drawPaddleBottom();
+	drawPaddleTop();
+	drawScore();
 	changeBallPosition();
 	if (rightPressed && paddleX < canvas.width - paddleWidth)
-		paddleX += 7;
+		paddleX += 10;
 	else if (leftPressed && paddleX > 0)
-		paddleX -= 7;
+		paddleX -= 10;
 }
 
 setInterval(draw, 10);
